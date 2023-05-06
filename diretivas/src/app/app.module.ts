49344fa92b7613d94ae71c484a8c634c7b705641
Pedrocardo005 <pedrocardo007@hotmail.com>
2023-05-05 22:17:47 -0300

    Aula 20 Angular CLI: ng lint, ng test, ng e2e

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
+import { Teste2Component } from './teste2/teste2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    TesteComponent
+    TesteComponent,
+    Teste2Component
   ],
   imports: [
     BrowserModule
